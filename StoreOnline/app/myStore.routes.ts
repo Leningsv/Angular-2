bd61bfc827d2d69e938f6e6da5a11d3bb99f3a57
Lenin <leningsv@hotmail.com>
2016-07-10 22:34:11 -0500

    comit

--- StoreOnline/app/myStore.routes.ts
+++ StoreOnline/app/myStore.routes.ts
@@ -1,6 +1,7 @@
 /**Importacion librerias para el routing */
 import { provideRouter, RouterConfig } from '@angular/router';
 import { Login }  from './login.component';
+import { Dashboard } from './dashboard.component';
 /**Importacion librerias routing avanzado */
 import { DashboardRoutes } from './dashboard.routes';
 export const routes: RouterConfig = [
